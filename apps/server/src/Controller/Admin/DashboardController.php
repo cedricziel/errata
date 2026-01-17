c6451ad5953e6a02f5efe08ee30868bf879af5b6
Cedric Ziel <cedric@cedric-ziel.com>
2026-01-18 00:59:47 +0100

    fix(admin): use AdminDashboard attribute for EasyAdmin route
    The admin route was not being registered due to EasyAdmin 4.24+
    requiring the #[AdminDashboard] attribute instead of #[Route].

--- apps/server/src/Controller/Admin/DashboardController.php
+++ apps/server/src/Controller/Admin/DashboardController.php
@@ -9,17 +9,17 @@ use App\Entity\Issue;
 use App\Entity\Organization;
 use App\Entity\Project;
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('ROLE_ADMIN')]
+#[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
         return $this->render('admin/dashboard.html.twig');
